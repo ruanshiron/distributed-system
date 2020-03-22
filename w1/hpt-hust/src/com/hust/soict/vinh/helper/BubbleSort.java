badb9f2c10510a4a5f247cbfcdeba6b631adef75
Vinh <theten12@gmail.com>
2020-03-22 20:11:19 +0700

    week 1

---
+++ w1/hpt-hust/src/com/hust/soict/vinh/helper/BubbleSort.java
@@ -0,0 +1,24 @@
+package com.hust.soict.vinh.helper;
+
+public class BubbleSort implements NumberSorter {
+
+    @Override
+    public void sort(int[] input) {
+        int inputLength = input.length;
+        int temp;
+
+        for (int i = 0; i < inputLength; i++) {
+
+
+            for (int j = 1; j < (inputLength - i); j++) {
+
+                if (input[j - 1] > input[j]) {
+                    temp = input[j - 1];
+                    input[j - 1] = input[j];
+                    input[j] = temp;
+                }
+
+            }
+        }
+    }
+}
---
+++ w1/hpt-hust/src/com/hust/soict/vinh/helper/NumberSorter.java
@@ -0,0 +1,5 @@
+package com.hust.soict.vinh.helper;
+
+public interface NumberSorter {
+    void sort(int arr[]);
+}
